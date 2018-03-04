9dd484eb591a36b75afa60063ff2365810ec8b80
Proverbs Xu <379548839@qq.com>
2018-03-04 18:18:53 +0800

    finished web-simulator.py

--- web-simulator.py
+++ web-simulator.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
 
 import requests
+import base64
 
 payload = {
-    'point_x': '',
-    'point_y': ''
+    'point_x': ['100', ],
+    'point_y': ['200'],
+    'resolution':['1920x1080', ],
+    'user_id':['10000', ],
+    'delay': ['250', ],
+    'device': ['MACHENIKE']
 }
 
 headers = {}
 headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'
 
-res = requests.post("https://127.0.0.1:18888/post", data=payload)
+
+# loop
+file_path = './pic-stream/mm.jpg'
+with open(file_path, 'rb') as img:
+    byte_content = img.read()
+
+base64_bytes = base64.b64encode(byte_content)
+base64_string = base64_bytes.decode('utf-8')
+
+payload['base64_image'] = ['base64,' + base64_string, ]
+
+# res = requests.get('https://127.0.0.1:18888/post', verify=True)
+res = requests.post("https://127.0.0.1:18888/post", data=payload, verify=False)
 
 print(res.text)
